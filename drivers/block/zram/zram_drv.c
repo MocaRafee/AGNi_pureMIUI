nathanchance/op6

zram: Use lz4 compression by default if present
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/block/zram/zram_drv.c
@@ -39,7 +39,11 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
+#ifdef CONFIG_CRYPTO_LZ4
+static const char *default_compressor = "lz4";
+#else
 static const char *default_compressor = "lzo";
+#endif
 
 /*
  * We don't need to see memory allocation errors more than once every 1

MocaRafee/AGNi_pureMIUI

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

drivers/block/zram/zram_drv.c
@@ -38,7 +38,11 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+static const char *default_compressor = "lz4";
+#else
 static const char *default_compressor = "lzo";
+#endif
 
 /*
  * We don't need to see memory allocation errors more than once every 1